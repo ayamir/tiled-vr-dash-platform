de83325e69deb291ff9dd42d2695d9f81fff20f6
ayamir <lgt986452565@gmail.com>
2022-01-04 12:54:07 +0800

    fix: enable multi_version bitrate for all tiles.

--- server/prepare/muti_version.py
+++ server/prepare/muti_version.py
@@ -1,7 +1,7 @@
 import cmder
 import os
 import utils
-from utils import workspace_dir
+from utils import video_output_dir
 from utils import TILE_HEIGHT
 from utils import TILE_WIDTH
 
@@ -13,32 +13,44 @@ def process(video_path, width, height, output_path, mark):
         os._exit(-1)
 
 
-def start_process():
+def start_process(workspace_dir):
     video_dirs = utils.get_dirs_in_path(workspace_dir)
-    print(f'There are {len(video_dirs)} videos in path {workspace_dir}')
+    if len(video_dirs) != 1:
+        print(
+            f'There are {len(video_dirs)} video directories in path {workspace_dir}')
+    else:
+        print(
+            f'There is {len(video_dirs)} video directory in path {workspace_dir}')
+
     for video_dir in video_dirs:
+        video_dir = workspace_dir + video_dir
+        cmder.infOut(f"Current video_dir is {video_dir}")
         tiled_video_root_dir = video_dir + "/tile_temp/"
         tiles = utils.get_files_in_path(tiled_video_root_dir)
-        for tile_name in tiles:
-            video_path = os.path.join(tiled_video_root_dir, tile_name)
-            output_dir = os.path.join(video_dir, tile_name.replace(".mp4", ""))
-            utils.create_dir(output_dir)
-            process(
-                video_path,
-                TILE_WIDTH,
-                TILE_HEIGHT,
-                os.path.join(
-                    output_dir,
-                    "L1.mp4"),
-                f'{tile_name.replace(".mp4", "").replace("_","-")}-L1')
-            process(
-                video_path,
-                TILE_WIDTH / 2,
-                TILE_HEIGHT / 2,
-                os.path.join(
-                    output_dir,
-                    "L0.mp4"),
-                f'{tile_name.replace(".mp4", "").replace("_","-")}-L0')
+        if len(tiles) == 0:
+            cmder.errorOut(f"There are no tiles in {tiled_video_root_dir}")
+        else:
+            for tile_name in tiles:
+                video_path = os.path.join(tiled_video_root_dir, tile_name)
+                output_dir = os.path.join(
+                    video_dir, tile_name.replace(".mp4", ""))
+                utils.create_dir(output_dir)
+                process(
+                    video_path,
+                    TILE_WIDTH,
+                    TILE_HEIGHT,
+                    os.path.join(
+                        output_dir,
+                        "L1.mp4"),
+                    f'{tile_name.replace(".mp4", "").replace("_","-")}-L1')
+                process(
+                    video_path,
+                    TILE_WIDTH / 2,
+                    TILE_HEIGHT / 2,
+                    os.path.join(
+                        output_dir,
+                        "L0.mp4"),
+                    f'{tile_name.replace(".mp4", "").replace("_","-")}-L0')
 
 
-start_process()
+start_process(video_output_dir)
