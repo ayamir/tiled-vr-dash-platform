dbc815da3b18039796c1893de84596b97523a0e8
ayamir <lgt986452565@gmail.com>
2022-01-05 12:12:08 +0800

    fix: generate mpd for base and tile.

--- server/prepare/dash_tiles.py
+++ server/prepare/dash_tiles.py
@@ -1,13 +1,26 @@
 import cmder
 import utils
+import os
 
 
 def dash_all_videos(video_output_dir):
     dirs = utils.get_dirs_path_in_path(video_output_dir)
     for dirpath in dirs:
-        print(dirpath)
+        cmder.infOut("Current video directory is " + dirpath)
+        # 生成base_dash.mpd
+        base_video_path = os.path.join(dirpath, "base.mp4")
+        base_mpd_path = os.path.join(dirpath, "base_dash.mpd")
         cmder.runCmd(
-            f'MP4Box -dash 1000 -rap -profile dashavc264:onDemand L0.mp4#video L1.mp4#video L0.mp4#audio')
+            f'MP4Box -dash 1000 -rap -profile dashavc264:onDemand {base_video_path} -out {base_mpd_path}')
+        tile_dirs = utils.get_dirs_path_in_path(dirpath)
+        for tile_dir in tile_dirs:
+            cmder.infOut("Current tile directory is " + tile_dir)
+            # 生成每个tile的两种版本的mpd
+            tile_L0_path = os.path.join(tile_dir, "L0.mp4")
+            tile_L1_path = os.path.join(tile_dir, "L1.mp4")
+            out_mpd_path = os.path.join(tile_dir, "L1_dash.mpd")
+            cmder.runCmd(
+                f'MP4Box -dash 1000 -rap -profile dashavc264:onDemand {tile_L1_path} {tile_L0_path} -out {out_mpd_path}')
 
 
 dash_all_videos(utils.video_output_dir)
