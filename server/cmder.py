f4f6ed93ebd899437860453a4c01774891dffb1b
ayamir <lgt986452565@gmail.com>
2022-01-11 11:50:57 +0800

    feat: output error result.

--- server/cmder.py
+++ server/cmder.py
@@ -2,45 +2,45 @@ import subprocess
 
 
 def redStr(str: str):
-    return ' \033[1;31m ' + str + ' \033[0m '
+    return " \033[1;31m " + str + " \033[0m "
 
 
 def greenStr(str: str):
-    return ' \033[1;32m ' + str + ' \033[0m '
+    return " \033[1;32m " + str + " \033[0m "
 
 
 def yelloStr(str: str):
-    return ' \033[1;33m ' + str + ' \033[0m '
+    return " \033[1;33m " + str + " \033[0m "
 
 
 def buleStr(str: str):
-    return ' \033[1;34m ' + str + ' \033[0m '
+    return " \033[1;34m " + str + " \033[0m "
 
 
 def errorOut(str: str):
-    print(redStr('[Error]: ' + str))
+    print(redStr("[Error]: " + str))
 
 
 def warningOut(str: str):
-    print(yelloStr('[Warning]: ' + str))
+    print(yelloStr("[Warning]: " + str))
 
 
 def successOut(str: str):
-    print(greenStr('[Success]: ' + str))
+    print(greenStr("[Success]: " + str))
 
 
 def infOut(str: str):
-    print(buleStr('[Info]: ' + str))
+    print(buleStr("[Info]: " + str))
 
 
 def runCmd(command: str):
-    infOut(command)
-    subp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    res = subp.communicate()
+    subp = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    res, err = subp.communicate()
     if subp.poll() == 0:
         successOut(command)
         return 0
     else:
-        errorOut(command)
+        errorOut(err.decode("utf-8"))
         return -1
